6e126f238e2b97c18d0121cdd16d1bd6b093f25f
Dharmesh Mahajan <dharmeshmahajan2@gmail.com>
2023-05-29 20:11:38 +0530

    Add functions

--- script.js
+++ script.js
@@ -1,10 +1,124 @@
-const numBtns = document.getElementsByClassName("numbers");
-const operationBtns = document.getElementsByClassName("operation");
-const equalsBtn = document.getElementsByClassName("equals");
-const delBtn = document.getElementsByClassName("delete");
-const clearBtn = document.getElementsByClassName("clear");
+const numBtns = document.querySelectorAll(".numbers");
+const operationBtns = document.querySelectorAll(".operation");
+const equalsBtn = document.querySelector(".equals");
+const delBtn = document.querySelector(".delete");
+const clearBtn = document.querySelector(".clear");
 
-const prev = document.getElementsByClassName("prev");
-const curr = document.getElementsByClassName("curr");
+const prev = document.querySelector(".prev");
+const curr = document.querySelector(".curr");
 
+window.addEventListener('keydown', keyboardSupport);
 
+const screen = document.getElementById("#screen");
+let currOperand='';
+let prevOperand='';
+let operation=undefined;
+
+function clear(){
+    curr.innerText='0'
+    prev.innerText=''
+    currOperand=''
+    prevOperand=''
+    operation = undefined
+}
+
+numBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        appendNum(button.innerText);
+        showResult();
+    })
+})
+
+operationBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        selectOperation(button.innerText);
+        showResult();
+    })
+})
+
+function selectOperation(op){
+    if(currOperand==='') return;
+    if(prevOperand !== ''){
+        operate();
+    }
+    operation = op
+    prevOperand = currOperand.toString() + operation.toString();
+    currOperand='' 
+}
+
+function appendNum(number){
+    if(number === '.' && currOperand.includes('.')) return;
+    currOperand = currOperand.toString() + number.toString();
+}
+
+function operate(){
+    let answer
+    const previous = parseFloat(prevOperand) 
+    const current = parseFloat(currOperand)
+    if(isNaN(previous) || isNaN(current)) return;
+
+    switch(operation){
+        case '+':
+            answer = previous + current;
+            break;
+        
+        case '-':
+            answer = previous - current;
+            break;
+
+        case '*':
+            answer = previous * current;
+            break;
+        
+        case '/':
+            if(current === 0) answer = null;
+            else answer = previous / current;
+            break;
+
+        case '^':
+            answer = Math.pow(previous,current);
+            break;
+        
+        default:
+            return;
+    }
+    currOperand = answer;
+    operation = undefined;
+    prevOperand = ''
+}
+
+function showResult(){
+    curr.innerText = currOperand
+    prev.innerText = prevOperand
+}
+
+clearBtn.addEventListener('click', () => { clear() });
+
+equalsBtn.addEventListener('click', () => {
+    operate();
+    showResult();
+})
+
+delBtn.addEventListener('click', ()=>{
+    remove();
+    showResult();
+})
+
+function remove(){
+    currOperand = currOperand.toString().slice(0,-1);
+}
+
+function keyboardSupport(e) {
+    if (e.key >= 0 && e.key <= 9 || e.key === '.') appendNum(e.key)
+    if (e.key === '=' || e.key === 'Enter') operate()
+    if (e.key === 'Backspace') remove()
+    if (e.key === 'Escape') clear()
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
+      selectOperation((e.key))
+
+    showResult();
+  }
+
+window.onload=()=>{
+    clear();
+}
