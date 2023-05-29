be5eac39c4251b626b2b82aee33c9a729f898c66
Dharmesh Mahajan <dharmeshmahajan2@gmail.com>
2023-05-29 16:09:50 +0530

    Select elements in javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+const numBtns = document.getElementsByClassName("numbers");
+const operationBtns = document.getElementsByClassName("operation");
+const equalsBtn = document.getElementsByClassName("equals");
+const delBtn = document.getElementsByClassName("delete");
+const clearBtn = document.getElementsByClassName("clear");
+
+const prev = document.getElementsByClassName("prev");
+const curr = document.getElementsByClassName("curr");
+
+
